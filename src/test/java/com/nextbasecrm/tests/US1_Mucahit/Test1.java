961085dba32846b8a46738b59a149ecf77dc6e41
mucro9 <mucht-ozr@hotmail.com>
2022-02-25 17:32:35 -0700

    initial project

---
+++ src/test/java/com/nextbasecrm/tests/US1_Mucahit/Test1.java
@@ -0,0 +1,10 @@
+package com.nextbasecrm.tests.US1_Mucahit;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void tes2(){
+        System.out.println("hi");
+    }
+}
