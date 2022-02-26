042ed8ae84ee2d5d99c5e872fc9a45a3cfb6e235
Nat <nattayasdet@gmail.com>
2022-02-25 20:14:40 -0500

    initial commit

---
+++ src/test/java/com/nextbasecrm/tests/US7_Nattaya/TestCase1.java
@@ -0,0 +1,10 @@
+package com.nextbasecrm.tests.US7_Nattaya;
+
+import org.testng.annotations.Test;
+
+public class TestCase1 {
+    @Test
+    public void test(){
+        System.out.println("My first test");
+    }
+}
